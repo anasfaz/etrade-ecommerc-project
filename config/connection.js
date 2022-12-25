698bc5f0fbbe427f7d5b61d2d6bccae564edd44a
anas pk <anaspkmmc@gmail.com>
2022-12-25 22:43:45 +0530

    mongo atls change

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,9 @@ const mongoClient=require('mongodb').MongoClient
 const state={
     db:null
 }
+
 module.exports.connect=(done)=>{
-    const url='mongodb://localhost:27017'
+    const url='mongodb+srv://anas:anas1122001@cluster0.08zmpwh.mongodb.net/?retryWrites=true&w=majority'
     const dbname='ecommerce'
      
     mongoClient.connect(url,(err,data)=>{
